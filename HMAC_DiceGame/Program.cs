aab3e4901d487cd5102417e933b447abf8badd25
d1amond <max.berd.1900@gmail.com>
2025-03-15 14:37:41 +0300

    Add project files.

---
+++ HMAC_DiceGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HMAC_DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
